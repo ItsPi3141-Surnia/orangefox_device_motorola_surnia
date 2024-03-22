222dceb918b4f42a5b48ef2ee5e5222a12b7a863
pi <piisthebestthingintheuniverse@gmail.com>
2024-03-21 20:55:34 -0700

    upbring for OrangeFox 12.1 + remove common tree + add prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,9 +16,47 @@
 
 DEVICE_PATH := device/motorola/surnia
 
-include device/motorola/msm8916-common/BoardConfigCommon.mk
+TARGET_ARCH := arm
+TARGET_CPU_ABI  := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
 
-TARGET_KERNEL_CONFIG := surnia_defconfig
+# Deprecated
+# TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+# TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+TARGET_BOARD_PLATFORM := msm8916
+
+TARGET_BOOTLOADER_BOARD_NAME := MSM8916
+TARGET_NO_BOOTLOADER := true
+
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 vmalloc=400M androidboot.bootdevice=soc.0  movablecore=160M androidboot.selinux=permissive
+
+# Prebuilt
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
+# We are using a prebuilt image so we don't need to build kernel
+# BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
+# TARGET_KERNEL_SOURCE := kernel/motorola/msm8916
+# TARGET_KERNEL_CONFIG := surnia_defconfig
+# BOARD_KERNEL_IMAGE_NAME := Image
+
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --dtb $(TARGET_PREBUILT_DTB)
+BOARD_RAMDISK_OFFSET := 0x01000000
+
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_USES_MOTOROLA_LOG := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+TARGET_RECOVERY_FSTAB := device/motorola/surnia/twrp.fstab
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
@@ -28,6 +66,26 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1895825408  # 1851392 * 1024 mmcblk0p42
 BOARD_PERSISTIMAGE_PARTITION_SIZE := 8388608    # 8192 * 1024 mmcblk0p31
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 5368578048 # 5242752 * 1024 mmcblk0p44
 
+# don't take forever to wipe
+BOARD_SUPPRESS_SECURE_ERASE := true
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+TW_INCLUDE_CRYPTO := true
+
 # TWRP
-DEVICE_RESOLUTION := 540x960
 TW_IGNORE_MAJOR_AXIS_0 := true
+
+# Screen
+DEVICE_RESOLUTION := 540x960
+TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 960
+
+TARGET_RECOVERY_PIXEL_FORMAT := RGB_565
+TW_NEW_ION_HEAP := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_IGNORE_MAJOR_AXIS_0 := true
+
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,54 @@
-add_lunch_combo omni_surnia-userdebug
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2021-2023 The OrangeFox Recovery Project
+#	
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+# 	
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="surnia"
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep \"$FDEVICE\")
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep \"$FDEVICE\")
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+    # Stanard stuff
+    export ALLOW_MISSING_DEPENDENCIES=true
+    export LC_ALL="C"
+
+    export FOX_VANILLA_BUILD=1
+
+   	export TW_DEFAULT_LANGUAGE="en"
+	export OF_USE_GREEN_LED=0
+    export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+    export TW_THEME=portrait_hdpi
+    export TW_MAX_BRIGHTNESS=255
+
+	# Screen
+    export OF_SCREEN_H=960
+    export OF_STATUS_H=36
+    export OF_STATUS_INDENT_LEFT=0
+    export OF_STATUS_INDENT_RIGHT=0
+fi
