0dac2601cb228da5da7167d8f06975995251d9f7
pi <piisthebestthingintheuniverse@gmail.com>
2024-03-22 17:34:04 -0700

    BoardConfig: change config to match official twrp android-9 branch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,27 +19,41 @@ DEVICE_PATH := device/motorola/surnia
 TARGET_ARCH := arm
 TARGET_CPU_ABI  := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a7
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_ARCH_VARIANT := armv8-a
 
 # Deprecated
 # TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 # TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+# Binder API version
+TARGET_USES_64_BIT_BINDER := true
 
 TARGET_BOARD_PLATFORM := msm8916
 
 TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 TARGET_NO_BOOTLOADER := true
 
-# BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 vmalloc=400M androidboot.bootdevice=7824900.sdhci movablecore=160M androidboot.selinux=permissive
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M androidboot.bootdevice=7824900.sdhci utags.blkdev=/dev/block/bootdevice/by-name/utags utags.backup=/dev/block/bootdevice/by-name/utagsBackup movablecore=160M androidboot.selinux=disabled
+# Sepolicy
+# It crashes without this, so don't remove
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
+
+BOARD_KERNEL_CMDLINE := \
+	androidboot.bootdevice=7824900.sdhci \
+	androidboot.hardware=qcom \
+	ehci-hcd.park=3 \
+	vmalloc=400M \
+	movablecore=160M \
+	androidboot.selinux=permissive \
+	enforcing=0 # same as selinux=permissive
 
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_KERNEL_SEPARATED_DT := true
+
 # These are just for reference
 # BOARD_KERNEL_OFFSET := 0x00008000
 # BOARD_KERNEL_SECOND_OFFSET := 0x00f00000
@@ -64,23 +78,28 @@ TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dt.img
 #        [ --pagesize <pagesize> ]
 #        [ --ramdisk_offset <address> ]
 #        [ --dt <filename> ]
-#        -o|--output <filename>
+#        -o|--output <filename> # don't worry about this one, it's in bootimg.mk
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) 
 # BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) --second_offset $(BOARD_KERNEL_SECOND_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
 BOARD_MKBOOTIMG_ARGS += --dt $(TARGET_PREBUILT_DTB)
-# Output is specified in bootimg.mk
 
+# Build boot img
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/bootimg.mk
+
+# Don't know why it's set to this date tbh
+PLATFORM_SECURITY_PATCH := 2029-10-01
 
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TARGET_USES_MOTOROLA_LOG := true
 
+# File system stuff
+TARGET_RECOVERY_FSTAB := device/motorola/surnia/twrp.fstab
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-
-TARGET_RECOVERY_FSTAB := device/motorola/surnia/twrp.fstab
+TW_INCLUDE_FUSE_EXFAT       := true # exFAT support
+TW_INCLUDE_FUSE_NTFS        := true # NTFS support
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
@@ -94,9 +113,13 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 5368578048 # 5242752 * 1024 mmcblk0p44
 BOARD_SUPPRESS_SECURE_ERASE := true
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 
+# Keymaster
+TARGET_PROVIDES_KEYMASTER := true
+
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 TW_INCLUDE_CRYPTO := true
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 
 # TWRP
 TW_IGNORE_MAJOR_AXIS_0 := true
@@ -105,12 +128,15 @@ TW_IGNORE_MAJOR_AXIS_0 := true
 DEVICE_RESOLUTION := 540x960
 TARGET_SCREEN_WIDTH := 540
 TARGET_SCREEN_HEIGHT := 960
+TARGET_RECOVERY_PIXEL_FORMAT := RGB_565
 
-# TARGET_RECOVERY_PIXEL_FORMAT := RGB_565
-TARGET_RECOVERY_PIXEL_FORMAT := RGBA_8888
+# Appearently this improves touch sensitivity
+TW_IGNORE_MAJOR_AXIS_0 := true
+
 TW_NEW_ION_HEAP := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_SCREEN_BLANK_ON_BOOT := true
-TW_IGNORE_MAJOR_AXIS_0 := true
+TW_USE_TOOLBOX := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
 
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
